da18f709a511957f4d63ed0364fb59b265e1f2b2
Lenkly2 <lenklygamebs@gmail.com>
2025-09-17 19:24:10 +0300

    forum-vrodi-work

--- forum/models.py
+++ forum/models.py
@@ -7,6 +7,9 @@ class Branch(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
 
+    def __str__(self):
+        return self.name
+
 class Massage(models.Model):
     branch = models.ForeignKey(Branch,on_delete=models.CASCADE)
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
--- forum/views.py
+++ forum/views.py
@@ -9,16 +9,20 @@ class BranchCreateView(CreateView):
     model = Branch
     template_name = "forum/branchcreate.html"
     form_class = BranchCreateForm
+    success_url = "../"
 
 class BranchListView(ListView):
     model = Branch
     template_name = "forum/branchlist.html"
+    context_object_name = "branchlist"
 
 class MassageCreateView(CreateView):
     model = Massage
     template_name = "forum/massagecreate.html"
     form_class = MassageCreateForm
+    success_url = "../"
 
 class MassageListView(ListView):
     model = Massage
     template_name = "forum/massagelist.html"
+    context_object_name = "massagelist"
